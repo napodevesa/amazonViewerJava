107ec0271f0a0fe1ad3c81ae31999d6cafe34f2f
ndev <napodevesa@gmail.com>
2020-08-20 13:55:36 +0200

    interfaz

--- AmazonViewer/src/com/ndev/amazonviewer/model/Magazine.java
+++ AmazonViewer/src/com/ndev/amazonviewer/model/Magazine.java
@@ -2,73 +2,26 @@ package com.ndev.amazonviewer.model;
 
 import java.util.Date;
 
-public class Magazine {
-
+public class Magazine extends Publication {
+	
 	private int id;
-	private String title;
-	private Date editionDate;
-	private String editorial;
-	private String[]autors;
 	
-	
-	public Magazine(String title, Date editionDate, String editorial) {
-		super();
-		this.title = title;
-		this.editionDate = editionDate;
-		this.editorial = editorial;
+	public Magazine(String title, Date edititionDate, String editorial) {
+		super(title, edititionDate, editorial);
+		// TODO Auto-generated constructor stub
 	}
-
-
+	
 	public int getId() {
 		return id;
 	}
-
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-
-	public String getTitle() {
-		return title;
-	}
-
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-
-	public Date getEditionDate() {
-		return editionDate;
-	}
-
-
-	public void setEditionDate(Date editionDate) {
-		this.editionDate = editionDate;
-	}
-
-
-	public String getEditorial() {
-		return editorial;
-	}
-
-
-	public void setEditorial(String editorial) {
-		this.editorial = editorial;
-	}
-
-
-	public String[] getAutors() {
-		return autors;
-	}
-
-
-	public void setAutors(String[] autors) {
-		this.autors = autors;
-	}
 	
-	
-	
-	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return  "\n :: MAGAZINE ::" + 
+				"\n Title: " + getTitle() +
+				"\n Editorial: " + getEditorial() + 
+				"\n Edition Date: " + getEdititionDate();
+	}
+
 }
